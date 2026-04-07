8d62b8e0b85cacf46a9f622d76901929c4ec3195
PorridgePower <>
2026-04-07 18:41:54 +0300

    Reversed hello

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	res := reverse.String("Hello, OTUS!")
+	fmt.Println(res)
 }
