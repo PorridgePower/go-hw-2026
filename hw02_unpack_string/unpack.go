62d2f89a2e068d244b32d75354127aa32fd4704f
PorridgePower <>
2026-04-23 23:10:31 +0300

    Working but not perfect

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if len(input) == 0 {
+		return "", nil
+	}
+	var ch rune
+	var cnt int
+	var result []rune
+	for _, c := range input {
+		if unicode.IsNumber(c) && ch != 0 {
+			cnt, _ = strconv.Atoi(string(c))
+			if cnt == 0 {
+				result = result[:len(result)-1]
+			} else {
+				for range cnt - 1 {
+					result = append(result, ch)
+				}
+			}
+			cnt = 0
+			ch = 0
+		} else if unicode.IsNumber(c) {
+			return "", ErrInvalidString
+		} else {
+			result = append(result, c)
+			ch = c
+		}
+	}
+
+	return string(result), nil
 }
