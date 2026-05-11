f3c285f01601de069b3103ec70a0fcf98a7ab227
PorridgePower <>
2026-05-11 23:56:30 +0300

    Implement list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,8 +19,92 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	length    int
+	frontItem *ListItem
+	backItem  *ListItem
+	lock      sync.Mutex
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+func (l *list) Front() *ListItem {
+	return l.frontItem
+}
+func (l *list) Back() *ListItem {
+	return l.backItem
+}
+func (l *list) PushFront(v interface{}) *ListItem {
+	l.lock.Lock()
+	newItem := ListItem{}
+	newItem.Value = v
+	if l.frontItem != nil {
+		newItem.Next = l.frontItem
+		l.frontItem.Prev = &newItem
+		l.frontItem = &newItem
+	} else {
+		l.frontItem = &newItem
+		l.backItem = &newItem
+	}
+	l.length += 1
+	l.lock.Unlock()
+	return &newItem
+}
+func (l *list) PushBack(v interface{}) *ListItem {
+	l.lock.Lock()
+	newItem := ListItem{}
+	newItem.Value = v
+	if l.backItem != nil {
+		newItem.Prev = l.backItem
+		l.backItem.Next = &newItem
+		l.backItem = &newItem
+	} else {
+		l.backItem = &newItem
+		l.frontItem = &newItem
+
+	}
+	l.length += 1
+	l.lock.Unlock()
+	return &newItem
+}
+
+// Метод вызывается только от существующих в списке элементов.
+func (l *list) Remove(i *ListItem) {
+	l.lock.Lock()
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.backItem = i.Prev
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.frontItem = i.Next
+	}
+	l.length -= 1
+	l.lock.Unlock()
+}
+
+// Метод вызывается только от существующих в списке элементов.
+func (l *list) MoveToFront(i *ListItem) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if i.Prev == nil {
+		return
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	if l.frontItem != nil {
+		l.frontItem.Prev = i
+	}
+	i.Prev = nil
+	i.Next = l.frontItem
+	l.frontItem = i
 }
 
 func NewList() List {
