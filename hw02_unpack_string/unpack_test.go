4cf08f8af334da08dbfade17690a23d6a81e9a77
PorridgePower <>
2026-05-11 21:20:49 +0300

    Add strings for tests

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -20,7 +20,6 @@ func TestUnpack(t *testing.T) {
 		{input: "aaф0b", expected: "aab"},
 		{input: "a1", expected: "a"},
 		{input: "🙃3", expected: "🙃🙃🙃"},
-		// uncomment if task with asterisk completed
 		{input: `qwe\4\5`, expected: `qwe45`},
 		{input: `qwe\45`, expected: `qwe44444`},
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
@@ -28,7 +27,6 @@ func TestUnpack(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -38,9 +36,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b", "0", "1111111", "1", "3🙃"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "0", "1111111", "1", "3🙃", "\\x", "x\\", "\\"}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
