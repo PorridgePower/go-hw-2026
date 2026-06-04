c4d8d8a5d7d565501580f702e7dc58ae99fcc0e9
PorridgePower <>
2026-06-04 23:50:46 +0300

    Implemented cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "fmt"
+
 type Key string
 
 type Cache interface {
@@ -9,8 +11,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +22,44 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	item := c.items[key]
+	isExist := (item != nil)
+	fmt.Printf("[LRU Set] key=%q exist=%v value=%v\n", key, isExist, value)
+
+	if !isExist {
+		if c.queue.Len() == c.capacity {
+			backItem := c.queue.Back()
+			fmt.Printf("[LRU Set] evict value=%v\n", backItem.Value)
+			c.queue.Remove(backItem)
+		}
+		newItem := c.queue.PushFront(value)
+		c.items[key] = newItem
+	} else {
+		item.Value = value
+		c.queue.MoveToFront(item)
+		return true
+
+	}
+	return isExist
+
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	var item *ListItem = c.items[key]
+	var isExist bool = (item != nil)
+	if !isExist {
+		return nil, isExist
+	}
+	c.queue.MoveToFront(item)
+	isExist = true
+	fmt.Printf("[LRU Get] key=%q found=%v value=%v\n", key, isExist, item.Value)
+	return item.Value, isExist
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
