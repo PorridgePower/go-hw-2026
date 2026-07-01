0f681fba3176038673fdf2d6448349f98c0eeb75
PorridgePower <62947325+PorridgePower@users.noreply.github.com>
2026-07-01 22:12:54 +0300

    [HW 03] Frequency analysis (#2)

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,37 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"cmp"
+	"fmt"
+	"maps"
+	"regexp"
+	"slices"
+	"strings"
+)
+
+var expr = regexp.MustCompile(`(?:[\pL\p{So}]|-)+`)
+
+func Top10(input string) []string {
+	result := make(map[string]int)
+
+	for _, w := range expr.FindAllString(input, -1) {
+		result[strings.ToLower(w)]++
+	}
+
+	delete(result, "-")
+
+	keys := slices.SortedFunc(maps.Keys(result), func(a, b string) int {
+		if result[a] != result[b] {
+			return cmp.Compare(result[b], result[a])
+		}
+		return cmp.Compare(a, b)
+	})
+
+	resLen := min(len(keys), 10)
+	res := make([]string, 0, resLen)
+	for _, k := range keys[:resLen] {
+		res = append(res, k)
+		fmt.Println(k, result[k])
+	}
+	return res
 }
