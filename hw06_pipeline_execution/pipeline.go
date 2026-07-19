19d84fdef8241ca6c13a47b65d4b285020683f25
PorridgePower <>
2026-07-20 00:52:00 +0300

    wrap job

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,15 +8,32 @@ type (
 
 type Stage func(in In) (out Out)
 
+func wrapper(done In, stage Stage) Stage {
+	wrapped := func(in In) (out Out) {
+		proxyChan := make(Bi)
+		go func() {
+			defer close(proxyChan)
+			for {
+				select {
+				case <-done:
+					return
+				case val, ok := <-in:
+					if !ok {
+						return
+					}
+					proxyChan <- val
+				}
+			}
+		}()
+		return stage(proxyChan)
+	}
+	return wrapped
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	firstChan := make(Bi)
 	var stageOutChan Out = firstChan
 	var stageInChan In = firstChan
-
-	for _, st := range stages {
-		stageOutChan = st(stageInChan)
-		stageInChan = In(stageOutChan)
-	}
 
 	starter := func() {
 		defer close(firstChan)
@@ -33,6 +50,11 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 		}
 	}
 	go starter()
+
+	for _, st := range stages {
+		stageOutChan = wrapper(done, st)(stageInChan)
+		stageInChan = In(stageOutChan)
+	}
 
 	return stageOutChan
 	// return nil ????
