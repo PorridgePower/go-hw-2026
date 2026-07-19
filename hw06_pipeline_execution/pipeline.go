8bcdd1960cfaf92ff07e20773941c87e640ae47e
PorridgePower <>
2026-07-19 23:35:57 +0300

    tmp 2

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,37 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	firstChan := make(Bi)
+	stageOutChan := firstChan
+	stageInChan := firstChan
+
+	for _, st := range stages {
+		stageOutChan := st(stageInChan)
+		stageInChan = stageOutChan
+	}
+
+	starter := func() {
+		for {
+			defer close(stageOutChan)
+			select {
+			case <-done:
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				firstChan <- val
+			}
+		}
+	}
+	go starter()
+
+	// background := func() {
+	// 	defer close(in)
+	// 	<-done
+	// }
+	// go background()
+
+	return stageOutChan
+	// return nil ????
 }
